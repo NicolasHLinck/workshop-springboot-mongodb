4edb66a8a23f11c51317ea92340107b40f968eeb
NicolasHLinck <nicolashlinck@gmail.com>
2025-11-10 14:24:54 -0300

    Instanciação da base de dados

--- src/main/java/com/nelioalves/workshopmongo/services/UserService.java
+++ src/main/java/com/nelioalves/workshopmongo/services/UserService.java
@@ -1,7 +1,7 @@
 package com.nelioalves.workshopmongo.services;
 
 import com.nelioalves.workshopmongo.domain.User;
-import com.nelioalves.workshopmongo.repository.UserResository;
+import com.nelioalves.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +11,7 @@ import java.util.List;
 public class UserService {
 
     @Autowired
-    private UserResository repo;
+    private UserRepository repo;
 
     public List<User> findAll(){
         return repo.findAll();

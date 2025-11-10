4d87cd7b59a9c89e6b35bca30a620cd9ddfcc7d4
NicolasHLinck <nicolashlinck@gmail.com>
2025-11-10 14:06:43 -0300

    Conectando ao mongoDB com repository e service

---
+++ src/main/java/com/nelioalves/workshopmongo/repository/UserResository.java
@@ -0,0 +1,13 @@
+package com.nelioalves.workshopmongo.repository;
+
+import com.nelioalves.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserResository extends MongoRepository<User, String> {
+
+
+
+
+}

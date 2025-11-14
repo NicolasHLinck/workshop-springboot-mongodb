12d585d1dda077d1cec740083dd97730251ab2c1
NicolasHLinck <nicolashlinck@gmail.com>
2025-11-13 23:07:22 -0300

    Obtendo post por id

---
+++ src/main/java/com/nelioalves/workshopmongo/services/PostService.java
@@ -0,0 +1,28 @@
+package com.nelioalves.workshopmongo.services;
+
+import com.nelioalves.workshopmongo.domain.Post;
+
+
+import com.nelioalves.workshopmongo.repository.PostRepository;
+
+import com.nelioalves.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+
+    public Post findById(String id) {
+         Optional<Post> user = repo.findById(id);
+         return user.orElseThrow(()-> new ObjectNotFoundException("Post não encontrado"));
+
+    }
+}
+
